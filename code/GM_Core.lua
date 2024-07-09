ca8f83190ef05af5b5ea0946ca1dd24b2beb82d7
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2024-07-09 18:20:09 +0200

    Fix luacheck warnings

--- code/GM_Core.lua
+++ code/GM_Core.lua
@@ -23,7 +23,7 @@
   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ]]--
 
--- luacheck: globals GetAddOnMetadata ChannelInfo C_Timer
+-- luacheck: globals GetAddOnMetadata ChannelInfo C_Timer C_Engraving
 
 rggm = rggm or {}
 local me = rggm
